f63b585a3d66e758be2763ba1a2477476723d5c9
Kautsar Hasby <kautsar20204@gmail.com>
2025-07-21 12:56:57 +0700

    fixing bug deployments

--- src/app/api/resumes/route.ts
+++ src/app/api/resumes/route.ts
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/prisma-client";
-import { IResumes } from "../../../../types";
+import { IResumes } from "@/types";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
--- src/hooks/useVerifiedOTP.tsx
+++
@@ -1,8 +0,0 @@
-import { FormState } from "@/lib/definition";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
-export const useVerifiedOTP = (verified: boolean) => {
-  const router = useRouter();
-  useEffect(() => {}, [router]);
-};
