32efb1b71c2d4acb8ba1233d763b27e790b677d0
Kautsar Hasby <kautsar20204@gmail.com>
2025-07-23 00:19:18 +0700

    fix:changin payload API Sign UP ver 2

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -2,49 +2,49 @@
 import { genericSignUp } from "@/lib/utils";
 import {
   FormState,
-  SignupFormApplicantSchema,
+  // SignupFormApplicantSchema,
   SignupFormUserSchema,
 } from "@/lib/definition";
-import { createSession } from "@/lib/session";
+// import { createSession } from "@/lib/session";
 
-export async function signUpApplicant(state: FormState, formData: FormData) {
-  let createdUser: { email: string; fullname: string } | null = null;
-  const result = await genericSignUp(state, formData, {
-    schema: SignupFormApplicantSchema,
-    createFn: async (data, tx) => {
-      const created = await tx.applicants.create({
-        data: {
-          email: data.email,
-          fullname: data.fullname,
-          birthdate: new Date(data.birthdate),
-          password: data.hashedPassword,
-          gender: data.gender,
-        },
-      });
+// export async function signUpApplicant(state: FormState, formData: FormData) {
+//   let createdUser: { email: string; fullname: string } | null = null;
+//   const result = await genericSignUp(state, formData, {
+//     schema: SignupFormApplicantSchema,
+//     createFn: async (data, tx) => {
+//       const created = await tx.applicants.create({
+//         data: {
+//           email: data.email,
+//           fullname: data.fullname,
+//           birthdate: new Date(data.birthdate),
+//           password: data.hashedPassword,
+//           gender: data.gender,
+//         },
+//       });
 
-      createdUser = {
-        email: created.email,
-        fullname: created.fullname,
-      };
-    },
-  });
+//       createdUser = {
+//         email: created.email,
+//         fullname: created.fullname,
+//       };
+//     },
+//   });
 
-  if (createdUser) {
-    const { email, fullname } = createdUser;
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/send-otp`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, fullname }),
-      }
-    );
-    if (res.status === 500) return;
-    await createSession({ email });
-  }
+//   if (createdUser) {
+//     const { email, fullname } = createdUser;
+//     const res = await fetch(
+//       `${process.env.NEXT_PUBLIC_BASE_URL}/api/send-otp`,
+//       {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({ email, fullname }),
+//       }
+//     );
+//     if (res.status === 500) return;
+//     await createSession({ email });
+//   }
 
-  return result;
-}
+//   return result;
+// }
 
 export async function signUpUser(state: FormState, formData: FormData) {
   return genericSignUp(state, formData, {
